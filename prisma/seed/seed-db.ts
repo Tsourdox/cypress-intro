787bc44f487902c4386aadffc8433199d1ee2349
David Jensen <tsourdox@icloud.com>
2025-09-02 15:32:28 +0200

    try again

--- prisma/seed/seed-db.ts
+++ prisma/seed/seed-db.ts
@@ -3,6 +3,13 @@ import { db } from "../db";
 import { seedTodos } from "./seed-todos";
 
 export async function seedDatabase(opts: { drop?: boolean } = {}) {
+  console.log("SEED DB", opts.drop, process.env.DATABASE_URL);
+  console.log("SEED DB", opts.drop, process.env.DATABASE_URL);
+  console.log("SEED DB", opts.drop, process.env.DATABASE_URL);
+  console.log("SEED DB", opts.drop, process.env.DATABASE_URL);
+  console.log("SEED DB", opts.drop, process.env.DATABASE_URL);
+  console.log("SEED DB", opts.drop, process.env.DATABASE_URL);
+  console.log("SEED DB", opts.drop, process.env.NODE_ENV);
   if (opts.drop) {
     await db.$runCommandRaw({ dropDatabase: 1 });
   }
